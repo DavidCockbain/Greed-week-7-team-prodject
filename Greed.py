5924e45ba90e14f43be00e08521e6d96589d94ea
DavidCockbain <98206594+DavidCockbain@users.noreply.github.com>
2022-02-25 23:16:06 +0200

    Update Greed.py

--- Greed.py
+++ Greed.py
@@ -1,22 +1,115 @@
-print("Greed")
+import turtle
 import random
 
-print("Greed")
+class Window:
+    main_window = turtle.Screen()
+    main_window.title("Greed")
+    main_window.bgcolor("black")
+    main_window.setup(width = 800, height = 600)
 
-class Falling_row:
-    def generate_row():
-        gem_row = [' '] * 100
-        gem = random.randint(0,99)
-        rock = random.randint(0,99)
-        gem_row[gem] = '*'
-        gem_row[rock] = 'O'
-        return gem_row
+class Player:
+    player = turtle.Turtle()
+    player.color("green")
+    hashtag = "turtle"
+    player.shape(hashtag)
+    player.penup()
+    player.speed(0)
+    player.setposition(0, -250)
+    playerspeed = 15
     
-    def print_row():
-        gem_row = Falling_row.generate_row()
-        for character in gem_row:
-            print(character, end = '')
+class Movement(Player):
+    def move_left(player,playerspeed):
+        x = player.xcor()
+        x -= playerspeed
+        if x < -390:
+            x = -390
+        player.setx(x)
 
-for i in range(15):
-    Falling_row.print_row()
+    def move_right(player,playerspeed):
+        x = player.xcor()
+        x += playerspeed
+        if x > 380:
+            x = 380
+        player.setx(x)
 
+    turtle.listen()
+    turtle.onkeypress(move_left, "Left")
+    turtle.onkeypress(move_right, "Right")
+
+
+class Gem:
+    gems = []
+
+    for _ in range(10):
+        gem = turtle.Turtle()
+        colors = ['gold', 'orange', 'red', 'maroon', 
+            'violet', 'magenta', 'purple', 'navy', 'blue', 
+            'cyan', 'turquoise', 'darkgreen']
+        gem.color(random.choice(colors))
+        asterisk = "triangle"
+        gem.shape(asterisk)
+        gem.penup()
+        gem.speed(0)
+        gem.setposition(random.randint(-400, 400), random.randint(-300, 300))
+        gem.speed = random.randint(10, 15)
+        gems.append(gem)
+
+class Rock:
+    rocks = []
+
+    for _ in range(10):
+        rock = turtle.Turtle()
+        rock.color("gray")
+        zero = "circle"
+        rock.shape(zero)
+        rock.penup()
+        rock.speed(0)
+        rock.setposition(random.randint(-400, 400), random.randint(-300, 300))
+        rock.speed = random.randint(10, 15)
+        rocks.append(rock)
+
+class Generate(Gem,Rock):
+    
+    while True:
+
+        for gem in Gem.gems:
+            y = gem.ycor()
+            y -= gem.speed
+            gem.sety(y)
+
+            if y < -300:
+                x = random.randint(-390, 380)
+                y = random.randint(300, 350)
+                gem.goto(x, y)
+
+        for rock in Rock.rocks:
+            y = rock.ycor()
+            y -= rock.speed
+            rock.sety(y)
+
+            if y < -300:
+                x = random.randint(-390, 380)
+                y = random.randint(300, 350)
+                rock.goto(x, y)
+
+
+
+class Catch:
+    pass
+
+class Scoreboard:
+    score = 0
+    pass
+
+def main(Window,Player,Movement):
+    Window().runall()
+    Player().runall()
+    Movement().runall()
+    Gem().runall()
+    Rock().runall()
+    Generate().runall()
+    
+
+if __name__ == "__main__":
+    main()
+    main_window.mainloop()
